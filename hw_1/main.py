e21efa1e9d47f7cf3f20d3f595f270f816ead669
IceGhostse <iceghostse@gmail.com>
2026-01-27 13:06:14 +0300

    реализовал class: Book

--- hw_1/main.py
+++ hw_1/main.py
@@ -23,9 +23,50 @@ class Animal:
         print(f'{self.name}: {sound}')
 
     def display_your_info(self):
-        print(f'Name: {self.name}\nSpecies: {self.species}\nAge: {self.age}')
+        print(f'Name: {self.name}\nSpecies: {self.species}\nAge: {self.age}\n')
 
 dog = Animal('Bobik', 'dog', 10)
 dog.make_sound('bark! bark!')
 dog.display_your_info()
 
+"""
+## **Задача №2.**
+
+Сформировать класс «Book» для представления сущности «Книга» в программе. 
+В качестве полей задаются: наименование книги (строка), автор книги (строка), количество страниц (число). 
+Реализовать операции: «отрыть» указанную страницу 
+(на вход в метод передается номер страницы и выводится строка, открылась страница или нет). 
+Реализовать метод вывода информации о книге на экран. 
+Метод вывода на экран должен аккумулировать состояние полей объекта.
+"""
+
+class Book:
+
+    title: str
+    author: str
+    pages: int
+
+    def __init__(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+    def open_page(self, page):
+
+        if page < 1:
+            raise ValueError('Page must be greater than 0')
+
+        print(f'Page open! Grats!\nPage: {page}')
+
+    def display_book_info(self):
+        print(f'Title: {self.title} | Author: {self.author} | Pages: {self.pages}')
+
+book = Book('The Adventures of Bobik', 'Bobik', 200)
+user_input = input('Enter a page: ')
+try:
+    user_input = int(user_input)
+    book.open_page(user_input)
+except ValueError:
+    print('Page not opened! Damn...')
+
+book.display_book_info()
