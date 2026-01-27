3319416412178b60007c624cdd67d1c0477f6633
IceGhostse <iceghostse@gmail.com>
2026-01-27 13:36:37 +0300

    реализовал class: PassengerPlane

--- hw_1/main.py
+++ hw_1/main.py
@@ -56,17 +56,81 @@ class Book:
         if page < 1:
             raise ValueError('Page must be greater than 0')
 
-        print(f'Page open! Grats!\nPage: {page}')
+        print(f'Page open! Grats!\nPage: {page} | Pages in book: {self.pages}')
 
     def display_book_info(self):
         print(f'Title: {self.title} | Author: {self.author} | Pages: {self.pages}')
 
 book = Book('The Adventures of Bobik', 'Bobik', 200)
-user_input = input('Enter a page: ')
+#user_input = input('Enter a page: ')
+user_input = 100
 try:
     user_input = int(user_input)
     book.open_page(user_input)
 except ValueError:
     print('Page not opened! Damn...')
-
 book.display_book_info()
+
+"""
+## **Задача №3.**
+
+Сформировать класс «**PassengerPlane**» для представления сущности «Пассажирский Самолет» в программе. 
+В качестве полей задаются: производитель самолета, модель самолета, вместимость, пассажиров, текущая высота, текущая скорость. 
+Реализовать следующие операции: взлет самолета, посадка самолета, изменение высоты, изменение скорости.  
+Реализовать метод вывода информации о пассажирском самолете на экран. Метод вывода на экран должен аккумулировать состояние полей объекта.
+
+Примечание:
+
+Взлет самолета – операция, которая выводит сообщение на консоль «Самолет взлетел!».
+
+Посадка самолета – операция, которая выводит сообщение на консоль «Самолет приземлился!».
+
+Изменение высоты – операция, которая на вход принимает новое значение высоты и заменяет старое.
+"""
+
+class PassengerPlane:
+
+    manufacturer: str
+    model: str
+    capacity: float
+    passengers: int
+    altitude: float
+    speed: float
+
+    def __init__(self, manufacturer, model, capacity, passengers, altitude, speed):
+        self.manufacturer = manufacturer
+        self.model = model
+        self.capacity = capacity
+        self.passengers = passengers
+        self.altitude = altitude
+        self.speed = speed
+
+    def takeoff(self):
+        if self.speed == 0:
+            self.speed = 300
+            self.change_altitude(500)
+            print('Самолет взлетел!')
+        else:
+            print("Самолет уже в воздухе!")
+
+    def touchdown(self):
+        if self.altitude > 0:
+            self.altitude = 0.0
+            self.speed = 0.0
+            print('Самолет приземлился!')
+        else:
+            print("Самолет уже на земле!")
+
+    def change_altitude(self, new_altitude: float):
+        self.altitude = new_altitude
+
+    def change_speed(self, new_speed: float):
+        self.speed = new_speed
+
+    def show_plane_info(self):
+        print(f'Manufacturer: {self.manufacturer}\nModel: {self.model}\nCapacity: {self.capacity} kg\nPassengers: {self.passengers} seats\nAltitude: {self.altitude} m\nSpeed: {self.speed} km/h')
+
+plane = PassengerPlane('Bobik – Engineering', 'Dogster', 15000, 150, 0, 0)
+plane.takeoff()
+plane.touchdown()
+plane.show_plane_info()
